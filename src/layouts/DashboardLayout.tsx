6c7b7bfb74756ac3c0cf44dd3fcd1fcecea094f3
DongPhuong <phuongnguyen20904@gmail.com>
2026-03-25 20:39:48 +0700

    fix(UI): Fix UI for register and sidebar

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -11,7 +11,7 @@ export default function DashboardLayout() {
       <SidebarInset>
         <header className='flex h-14 shrink-0 items-center gap-2 border-b bg-white px-4'>
           <SidebarTrigger className='-ml-1' />
-          <Separator orientation='vertical' className='mr-2 h-4' />
+          <Separator orientation='vertical' className='bg-gray-200 mr-2 h-4' />
           <div className='flex-1' />
         </header>
 
