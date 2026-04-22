d8ed4d3a78ffe3912a5c2dd706566b67c7bf8b7d
MinhDuy <duylm2016@gmail.com>
2026-04-22 20:29:31 +0700

    feat: integrate api profile page

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { userService } from '@/services/user.service';
 import { Button } from '@/components/ui/button';
